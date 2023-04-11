920531d97eb5c51f404ca90d0d70d12950160137
Raven <89488590+AnaBeatrizAdrianodaSilva@users.noreply.github.com>
2023-04-11 14:52:00 -0300

    Update RMGameScreens.jsx

--- src/screens/RMGameScreens.jsx
+++ src/screens/RMGameScreens.jsx
@@ -73,7 +73,7 @@ export default function RMGamerScreens() {
 
       <View>
         {resposta !== null && (
-          <Text>{resposta ? "Sim" : "Não"}</Text>
+          <Text>{resposta ? "Vivaço" : "Morreu"}</Text>
         )}
       </View>
 
